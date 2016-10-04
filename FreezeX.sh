570700e1d2576562d76d10af009f3138ceec28d3
Love Borgström <loveborgstrom98@gmail.com>
2016-10-04 09:45:58 +0200

    Add files via upload

--- FreezeX.sh
+++ FreezeX.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 # License: The MIT License (MIT)
 # Author Zuzzuc https://github.com/Zuzzuc/
-#
-# Do NOT use with "set -e", or SFS will fail.
-#
-l=0 && I=0 && klc=0
+l=0 && I=0 && klc=0 && KM="STOP"
 s=($@)
 sz=${#s[@]}
 kl=($(ps -A -o pid))
 klz=${#kl[@]}
 i=${kl[(1)]}
+if [ "$1" == "-1" ];then
+	KM="CONT"
+fi
 FS() {
-	kill -STOP $i > /dev/null 2>&1
+	kill -$KM $i > /dev/null 2>&1
 }
 SFS() {
 	il=0
 	while [ $il -le $sz ];do
 		if [ "$1" == "${s[$il]}" ];then
-			return 0
+			echo 0 && return 0
 		fi
 		il=$((il+1))
 	done
-	return 1
+	echo 1 && return 1
 }
 while : ;do
 	if [ $l -ge $klz ];then
@@ -29,7 +29,7 @@ while : ;do
 	else
 		if [ $i != $$ ];then
 			if [ $I -le $sz ];then		
-				if [ "$(SFS $i;echo $?)" == "0" ];then
+				if [ "$(SFS $i)" == "0" ];then
 					I=$(($I+1))
 				else
 					FS
